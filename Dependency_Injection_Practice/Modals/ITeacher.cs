4bed7c3a417f1802bcebec77166d1c80ad7441f1
cemil3535 <cemilozcan35@gmail.com>
2024-10-17 17:44:10 +0300

    Add project files.

---
+++ Dependency_Injection_Practice/Modals/ITeacher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Injection_Practice.Modals
+{
+    public interface ITeacher
+    {
+        string GetInfo();
+    }
+}
---
+++ Dependency_Injection_Practice/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Dependency_Injection_Practice.Modals;
+using System.Net.Http.Headers;
+
+
+// Creating a Teacher object
+ITeacher teacher = new Teacher("Cemal","Kaya");
+
+// Create ClassRoom object and pass Teacher object
+ClassRoom classroom = new ClassRoom(teacher);
+
+// Printing teacher's information
+Console.WriteLine(classroom.GetTeacherInfo());
