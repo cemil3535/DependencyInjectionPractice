3b9065c714672d0c082f478e16348eef0c0c45cb
Cemil Ozcan <71188464+cemil3535@users.noreply.github.com>
2024-10-20 14:49:23 +0300

    Add files via upload

---
+++ DependencyInjectionPractice/DependencyInjectionPractice/Managers/ClassRoom.cs
@@ -0,0 +1,19 @@
+﻿using DependencyInjectionPractice.Services;
+
+namespace DependencyInjectionPractice.Managers
+{
+    public class ClassRoom
+    {
+        private readonly ITeacher _teacher;
+
+        public ClassRoom(ITeacher teacher)
+        {
+            _teacher = teacher;
+        }
+
+        public void GetTeacherInfo()
+        {
+             _teacher.GetInfo();
+        }
+    }
+}
